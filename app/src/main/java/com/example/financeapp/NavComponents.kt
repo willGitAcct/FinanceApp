663d72912d0e1993935705fac4ee1986da737acb
WillGitAcct <willjhub@outlook.com>
2022-09-01 11:58:12 -0400

    added history page

--- app/src/main/java/com/example/financeapp/NavComponents.kt
+++ app/src/main/java/com/example/financeapp/NavComponents.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
@@ -19,10 +21,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -207,11 +213,18 @@ fun LearningPage(){
 
 @Composable
 fun History(){
-    Box(modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center){
-        Text(text = "History")
-    }
+
+        Column(Modifier.verticalScroll(rememberScrollState())
+            ){
+
+            Text(text= stringResource(R.string.History1), fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)
+            Image(painter = painterResource(id = R.drawable.amsterdam), contentDescription =null , Modifier.align(Alignment.CenterHorizontally))
+            Text(text = stringResource(id = R.string.History2), fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)
+
+        }
+
 }
+
 @Composable
 fun Basics(){
     Box(modifier = Modifier.fillMaxSize(),
