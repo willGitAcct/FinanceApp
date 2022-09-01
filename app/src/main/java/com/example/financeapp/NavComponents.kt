ee6bdec9d7b374391c528c4eee7e6eeab13bdafb
WillGitAcct <willjhub@outlook.com>
2022-09-01 10:46:12 -0400

    changed learning page to top nav bar; added 4 template pages

--- app/src/main/java/com/example/financeapp/NavComponents.kt
+++ app/src/main/java/com/example/financeapp/NavComponents.kt
@@ -23,9 +23,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 import com.example.financeapp.ui.Screen
+import com.example.financeapp.ui.TopNavHost
+import com.example.financeapp.ui.TopNavScreen
 
 @Composable
 fun HomePage(){
@@ -185,51 +189,47 @@ fun CalculatorPage(){
 
 
 
-
+@Preview
 @Composable
 fun LearningPage(){
+    val listItems = listOf(Screen.History2, Screen.Basics, Screen.Types, Screen.Advanced)
+    val navController = rememberNavController()
     Box(modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center){
-        Text(text = "Academy")
-        Column {
-            Button(onClick = {},
-                modifier = Modifier.fillMaxWidth().padding(12.dp)
-                    .height(120.dp)
-                    .background(Color.Transparent),
-                shape = RoundedCornerShape(8.dp),
-                //contentPadding = PaddingValues(12.dp)
-
-            ){
-                Image(painter = painterResource(id = R.drawable.history), contentDescription ="History"
-                , contentScale = ContentScale.FillBounds,
-                modifier = Modifier.fillMaxSize())
-            }
-
-            Button(onClick = {},
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
-                ){
-                Image(painter = painterResource(id = R.drawable.investbasics), contentDescription ="History" )
-            }
-
-            Button(onClick = {},
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
-
-
-            ){
-                Image(painter = painterResource(id = R.drawable.types), contentDescription ="History" )
-            }
-
-            Button(onClick = {},
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
-
-
-            ){
-                Image(painter = painterResource(id = R.drawable.inter), contentDescription ="History" )
-            }
-
+        Text(text = "Stars")
+        Scaffold(topBar = {TopNavScreen(navController = navController, item = listItems)
+        }) {
+            TopNavHost(topNavHostController = navController)
         }
+    }
+
+}
+
+@Composable
+fun History(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(text = "History")
+    }
+}
+@Composable
+fun Basics(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(text = "Bascs")
+    }
+}
+@Composable
+fun Types(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(text = "Types")
+    }
+}
+@Composable
+fun Advanced(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(text = "Advanced concepts")
     }
 }
--- app/src/main/java/com/example/financeapp/ui/Navigation.kt
+++ app/src/main/java/com/example/financeapp/ui/Navigation.kt
@@ -19,6 +19,10 @@ sealed class Screen(val title:String, val route: String, @DrawableRes val icons:
     object Calculator:Screen(title = "Calculator", "calc_route", icons = com.example.financeapp.R.drawable.ic_baseline_calculate_24)
     object History:Screen(title = "Learning", "learning_route", icons = com.example.financeapp.R.drawable.ic_baseline_history_edu_24)
 
+    object History2:Screen(title = "History", "history_route", icons = com.example.financeapp.R.drawable.ic_baseline_history_edu_24)
+    object Basics:Screen(title = "Basics", "basics_route", icons = com.example.financeapp.R.drawable.ic_baseline_monetization_on_24)
+    object Types:Screen(title = "Types", "types_route", icons = com.example.financeapp.R.drawable.ic_baseline_bar_chart_24)
+    object Advanced:Screen(title = "Advanced", "adv_route", icons = com.example.financeapp.R.drawable.ic_baseline_functions_24)
 
 }
 
@@ -33,6 +37,50 @@ fun BottomNavHost(navHostController: NavHostController){
         }
         composable(route = Screen.History.route){
             LearningPage()
+        }
+    }
+}
+
+//or the top nav in learning page
+@Composable
+fun TopNavHost(topNavHostController: NavHostController){
+    NavHost(navController = topNavHostController, startDestination = Screen.History2.route){
+        composable(route = Screen.History2.route){
+            History()
+        }
+        composable(route = Screen.Basics.route){
+            Basics()
+        }
+        composable(route = Screen.Types.route){
+            Types()
+        }
+        composable(route = Screen.Advanced.route){
+            Advanced()
+        }
+    }
+}
+
+@Composable
+fun TopNavScreen(navController: NavController, item:List<Screen>){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()//whenever changed get the state insideteh backstack
+    val currentDest= navBackStackEntry?.destination
+    BottomNavigation() {
+        item.forEach { screen ->
+            BottomNavigationItem(selected = currentDest?.route == screen.route ,
+                onClick = {
+                    navController.navigate(screen.route){
+                        launchSingleTop = true
+                        popUpTo(navController.graph.findStartDestination().id){
+                            saveState = true
+                        }
+                        restoreState = true //if any state here, it will be restored. if the above isnt saved, then this restore wont have an effect
+                    }
+
+                },
+                icon ={ Icon(painter = painterResource(id = screen.icons), contentDescription = null)},
+                label ={Text(screen.title)},
+                alwaysShowLabel = false,
+                selectedContentColor = MaterialTheme.colors.secondary)
         }
     }
 }
